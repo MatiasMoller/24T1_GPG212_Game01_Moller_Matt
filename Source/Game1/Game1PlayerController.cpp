3fa58bf9fdee05419a1f7bf701eea473b985ceb8
Moller_Matt_Laptop <136556009+MatiasMoller@users.noreply.github.com>
2024-02-09 11:48:19 +1100

    Initial commit

---
+++ Source/Game1/Game1PlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Game1PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void AGame1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
